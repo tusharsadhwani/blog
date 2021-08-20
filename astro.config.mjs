983b60684c5c3c06e840c000cb84f1c4ad3c38c5
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-08-20 21:25:27 +0530

    Use absolute URLs for link preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
+    site: "https://sadh.life",
+    sitemap: true,
   },
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on. 
+    // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
     // tailwindConfig: '',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
   },
