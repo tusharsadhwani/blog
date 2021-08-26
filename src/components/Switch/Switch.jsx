3c49d3361b9c912812cba1497abe3eac3b242e3e
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-08-26 23:56:06 +0530

    Make switch into react component

---
+++ src/components/Switch/Switch.jsx
@@ -0,0 +1,37 @@
+import "./Switch.module.css";
+
+const cssVariables = [
+  "--background-color",
+  "--highlight-color",
+  "--border-color",
+  "--primary-color",
+  "--link-color",
+  "--active-color",
+  "--subheading-color",
+  "--shadow-color",
+  "--text-color",
+];
+
+const Switch = () => {
+  const switchId = "switch-" + Math.floor(Math.random() * 10000);
+  const toggleTheme = () => {
+    const root = document.documentElement;
+    const computedCssVars = window.getComputedStyle(root);
+    const textColor = computedCssVars.getPropertyValue("--text-color").trim();
+    const currentTheme = textColor === "white" ? "dark" : "light";
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    console.log({ textColor, currentTheme, newTheme });
+
+    for (const cssVar of cssVariables) {
+      root.style.setProperty(cssVar, `var(${cssVar}-${newTheme})`);
+    }
+  };
+  return (
+    <div style={{ display: "flex" }}>
+      <input id={switchId} type="checkbox" onClick={toggleTheme} />
+      <label for={switchId}></label>
+    </div>
+  );
+};
+
+export default Switch;
