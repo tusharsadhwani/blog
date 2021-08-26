26abd91f6bb38c4a4864c79395c4a36722d57b4a
tusharsadhwani <tusharsadhwani@users.noreply.github.com>
2021-08-26 18:27:39 +0000

    deploy: e20b23676860b9c19f3205f07642959ddf65f648

---
+++ _snowpack/pkg/@astrojs.renderer-preact.client.v0.2.1.js
@@ -0,0 +1,1 @@
+import{v as a,S as e}from"../../preact.v10.5.14-48b2ca28.js";const t=({value:e})=>e?a("astro-fragment",{dangerouslySetInnerHTML:{__html:e}}):null;t.shouldComponentUpdate=()=>!1;var o=o=>(r,l,n)=>e(a(r,l,a(t,{value:n})),o);export{o as default};
