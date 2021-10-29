5a8876ff38e11b0aa5bdb99672f81ce0d8c5b744
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-10-30 00:54:43 +0530

    Simplify theme logic

--- src/components/ThemeSwitch/Switch.jsx
+++ src/components/ThemeSwitch/Switch.jsx
@@ -1,28 +1,13 @@
 import classes from "./Switch.module.css";
 
-const cssVariables = [
-  "--background-color",
-  "--highlight-color",
-  "--border-color",
-  "--primary-color",
-  "--link-color",
-  "--active-color",
-  "--subheading-color",
-  "--shadow-color",
-  "--text-color",
-];
-
 const Switch = () => {
   const switchId = "switch-" + Math.floor(Math.random() * 10000);
   const toggleTheme = () => {
     const root = document.documentElement;
-    const computedCssVars = window.getComputedStyle(root);
-    const textColor = computedCssVars.getPropertyValue("--text-color").trim();
-    const currentTheme = textColor === "white" ? "dark" : "light";
-    const newTheme = currentTheme === "dark" ? "light" : "dark";
-
-    for (const cssVar of cssVariables) {
-      root.style.setProperty(cssVar, `var(${cssVar}-${newTheme})`);
+    if (root.dataset.theme === "light") {
+      root.dataset.theme = "dark";
+    } else {
+      root.dataset.theme = "light";
     }
   };
   return (
