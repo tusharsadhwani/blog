7d7b3c45551536d9e31e5c824b1db5faa416edeb
tusharsadhwani <tusharsadhwani@users.noreply.github.com>
2021-11-21 16:04:49 +0000

    deploy: 2d458b44ca8c3f123bf69fbd771f44dd93a242a5

---
+++ assets/client.58da5f10.js
@@ -0,0 +1,1 @@
+import{v as e,S as o}from"./preact.module.3b40a332.js";const r=({value:t})=>t?e("astro-fragment",{dangerouslySetInnerHTML:{__html:t}}):null;r.shouldComponentUpdate=()=>!1;var m=r,u=t=>(a,l,n)=>o(e(a,l,e(m,{value:n})),t);export{u as default};
