67623c66912bca8edddcf97850fd92756332e11c
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2022-01-26 22:12:41 +0530

    Store dark/light mode in localhost

--- src/components/ThemeSwitch/Switch.jsx
+++ src/components/ThemeSwitch/Switch.jsx
@@ -6,13 +6,20 @@ const Switch = () => {
     const root = document.documentElement;
     if (root.dataset.theme === "light") {
       root.dataset.theme = "dark";
+      localStorage.setItem("lightMode", "false");
     } else {
       root.dataset.theme = "light";
+      localStorage.setItem("lightMode", "true");
     }
   };
   return (
     <div className={classes.themeSwitch} style={{ display: "flex" }}>
-      <input id={switchId} type="checkbox" onClick={toggleTheme} />
+      <input
+        id={switchId}
+        type="checkbox"
+        onClick={toggleTheme}
+        checked={document.documentElement.dataset.theme === "light"}
+      />
       <label for={switchId}></label>
     </div>
   );
